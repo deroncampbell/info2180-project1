bd50c2932bd573a3785e8628be2384387ae2ac45
Deron Campbell <deroncampbell88@gmail.com>
2020-11-01 23:33:37 -0500

    Added all Project 1 Files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
 /* Add your JavaScript to this file */
+window.onload = function(){
+    const form = document.forms[0];
+
+    form.onsubmit = function(e){
+
+        var result = document.getElementById('email');
+        alert("FORM SUBMITTED!");
+    };
+
+}
